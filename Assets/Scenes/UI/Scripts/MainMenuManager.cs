f61fd622f1d56e0d2d021f17e8d1024dfb1f2132
Gleb Plaxienko <gleb.plaxienko@gmail.com>
2023-12-08 03:30:43 +0700

    Add menu navigation

---
+++ Assets/Scenes/UI/Scripts/MainMenuManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void GoMain()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void GoSetting()
+    {
+        SceneManager.LoadScene("MainSettings");
+    }
+
+    public void GoFAQ()
+    {
+        SceneManager.LoadScene("MainFAQ");
+    }
+    
+    public void GoStory()
+    {
+        SceneManager.LoadScene("StoryMode");
+    }
+
+    public void GoChallenger()
+    {
+        SceneManager.LoadScene("ChallengerMode");
+    }
+
+    public void GoEditor()
+    {
+        SceneManager.LoadScene("EditorMode");
+    }
+}
