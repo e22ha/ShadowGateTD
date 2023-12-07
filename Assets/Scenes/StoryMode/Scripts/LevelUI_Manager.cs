c219509f7305699223e3186d041a0b09e9020f92
Gleb Plaxienko <gleb.plaxienko@gmail.com>
2023-12-08 06:21:35 +0700

    Add level UI on testlevel

---
+++ Assets/Scenes/StoryMode/Scripts/LevelUI_Manager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelUI_Manager : MonoBehaviour
+{
+    public GameObject pauseUI;
+    public GameObject settingsMenu;
+    public GameObject pauseMenu;
+    public GameObject pauseBtn;
+    
+    public void pauseMenuOn()
+    {
+        pauseUI.SetActive(true);
+        pauseBtn.SetActive(false);
+    }
+
+    public void pauseMenuOff()
+    {
+        settingsMenu.SetActive(false);
+        pauseUI.SetActive(false);
+        pauseBtn.SetActive(true);
+    }
+
+    public void settingsMenuOn()
+    {
+        pauseMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+    }
+
+    public void settingsMenuOff()
+    {
+        settingsMenu.SetActive(false);
+        pauseMenu.SetActive(true);
+    }
+
+    public void goMain()
+    {
+
+    }
+
+    public void restartGame()
+    {
+
+    }
+}
