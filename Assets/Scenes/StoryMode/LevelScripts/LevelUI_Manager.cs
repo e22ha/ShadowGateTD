399b36843ffad8cee7e6fac15d1f06d591253222
Gleb Plaxienko <gleb.plaxienko@gmail.com>
2024-01-24 10:23:53 +0700

    Layer ordering hotfix

--- Assets/Scenes/StoryMode/LevelScripts/LevelUI_Manager.cs
+++ Assets/Scenes/StoryMode/LevelScripts/LevelUI_Manager.cs
@@ -8,9 +8,10 @@ public class LevelUI_Manager : MonoBehaviour
 {
     public GameObject pauseUI;
     public GameObject settingsMenu;
+    public Canvas canvasUI;
     public GameObject pauseMenu;
     public GameObject pauseBtn;
-    
+
     public int levelIndex;
 
     public void pauseMenuOn()
@@ -19,6 +20,7 @@ public class LevelUI_Manager : MonoBehaviour
 
         pauseUI.SetActive(true);
         pauseBtn.SetActive(false);
+        canvasUI.sortingOrder = 2;
     }
 
     public void pauseMenuOff()
@@ -28,6 +30,7 @@ public class LevelUI_Manager : MonoBehaviour
         settingsMenu.SetActive(false);
         pauseUI.SetActive(false);
         pauseBtn.SetActive(true);
+        canvasUI.sortingOrder = 0;
     }
 
     public void settingsMenuOn()
