879f883136b2230de2a2d6da2976448cc4de74ba
Sergey Manzhos <sergey.manzhos@outlook.com>
2024-01-24 11:39:17 +0700

    Update DiamondManger.cs

--- Assets/DiamondManger.cs
+++ Assets/DiamondManger.cs
@@ -6,16 +6,23 @@ public class DiamondManger : MonoBehaviour
 {
     private const string DiamondsPlayerPrefsKey = "PlayerDiamonds";
 
+    private static bool _isFirst = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        PlayerPrefs.SetInt(DiamondsPlayerPrefsKey, 5);
-        PlayerPrefs.Save();
+        if (_isFirst)
+        {
+            Debug.Log("DeleteAll");
+            PlayerPrefs.DeleteAll();
+            PlayerPrefs.SetInt(DiamondsPlayerPrefsKey, 5);
+            PlayerPrefs.Save();
+            _isFirst = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
